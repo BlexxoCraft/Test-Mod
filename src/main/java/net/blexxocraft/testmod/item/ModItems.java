6c2f7b1962ec04730a48ba9137ad9eb9c898dcf6
BlexxoCraft <christian.bauer2004@gmail.com>
2022-04-14 22:02:06 +0200

    Added Dowsing Rod

--- src/main/java/net/blexxocraft/testmod/item/ModItems.java
+++ src/main/java/net/blexxocraft/testmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.blexxocraft.testmod.item;
 
 import net.blexxocraft.testmod.TestMod;
+import net.blexxocraft.testmod.item.custom.DowsingRodItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,6 +14,8 @@ public class ModItems {
     public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
 
     public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
+
+    public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod", () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB).durability(16)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
