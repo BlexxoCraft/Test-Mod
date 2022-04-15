8386b5c87c12d3297ee8c12fe0086c7df59c59b8
BlexxoCraft <christian.bauer2004@gmail.com>
2022-04-15 20:49:56 +0200

    Added Coal Coke (custom fuel item)

--- src/main/java/net/blexxocraft/testmod/item/ModItems.java
+++ src/main/java/net/blexxocraft/testmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.blexxocraft.testmod.item;
 
 import net.blexxocraft.testmod.TestMod;
+import net.blexxocraft.testmod.item.custom.CoalCokeItem;
 import net.blexxocraft.testmod.item.custom.DowsingRodItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,6 +19,8 @@ public class ModItems {
     public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod", () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB).durability(16)));
 
     public static final RegistryObject<Item> CUCUMBER = ITEMS.register("cucumber", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB).food(ModFoods.CUCUMBER)));
+
+    public static final RegistryObject<Item> COAL_COKE = ITEMS.register("coal_coke", () -> new CoalCokeItem(new Item.Properties().tab(ModCreativeModeTab.TESTMOD_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
