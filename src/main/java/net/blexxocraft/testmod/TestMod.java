230fac75d687042c4f6ccfa1ea7ba06add53ccba
BlexxoCraft <christian.bauer2004@gmail.com>
2022-04-17 20:31:02 +0200

    Added Citrine Buttons and Pressure Plates and Ebony Doors and Trapdoors

--- src/main/java/net/blexxocraft/testmod/TestMod.java
+++ src/main/java/net/blexxocraft/testmod/TestMod.java
@@ -3,10 +3,13 @@ package net.blexxocraft.testmod;
 import com.mojang.logging.LogUtils;
 import net.blexxocraft.testmod.block.ModBlocks;
 import net.blexxocraft.testmod.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -24,6 +27,7 @@ public class TestMod
     {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
 
         ModItems.register(eventBus);
         ModBlocks.register(eventBus);
@@ -37,5 +41,10 @@ public class TestMod
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_DOOR.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_TRAPDOOR.get(), RenderType.translucent());
     }
 }
